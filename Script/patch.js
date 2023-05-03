dde056011af0395a4ac7c4775852de2783583816
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-05-03 14:07:10 -0400

    Patch Notes

--- Script/patch.js
+++ Script/patch.js
@@ -1,3 +1,5 @@
+// const { data } = require("cheerio/lib/api/attributes");
+
 document.addEventListener("DOMContentLoaded", function() {
   var btn = document.getElementsByClassName("collapse");
 
@@ -12,3 +14,26 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+//display champion image//
+const apiKey = "RGAPI-6697e72d-349c-4059-8289-21d461a7db46";
+
+function fetchChampionImage(championName) {
+  fetch(`https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/${championName}.png?api_key=${apiKey}`)
+    .then(response => {
+      if (response.ok) {
+        return response.blob();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then(blob => {
+      const imageUrl = URL.createObjectURL(blob);
+      const imageTag = `<img src="${imageUrl}" alt="${championName}">`;
+      document.getElementById("championImage").innerHTML = imageTag;
+    })
+    .catch(error => {
+      console.error("Error fetching champion image:", error);
+    });
+}
+
+// Example usage:
+fetchChampionImage("Neeko");
