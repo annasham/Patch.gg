a4de2862bb9e5860661d22ebba27848a178028d0
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-05-04 19:07:46 -0400

    Patch 13.9.1 Notes

--- Script/patch.js
+++ Script/patch.js
@@ -1,4 +1,3 @@
-// const { data } = require("cheerio/lib/api/attributes");
 //fetch data
 fetch('https://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion/Neeko.json')
   .then(response => response.json())
@@ -55,29 +54,4 @@ function fetchChampionImage(championName) {
 
 // Example usage:
 fetchChampionImage("Neeko");
-
-
-// //fetch data
-// const fetch = require("node-fetch");
-// const cheerio = require("cheerio");
-
-// //get Data
-// const getRiotData = (URL) => {
-//     return fetch(URL)
-//     .then((response) => response.text())
-//     .then((data) => {
-//         return data;
-//     });
-// };
-
-// //patch notes URL
-// const URL = "https://www.leagueoflegends.com/en-us/news/game-updates/patch-13-9-notes/";
-
-// //run program
-// const getPatchData = async () => {
-//     const patchData = await getRiotData(URL);
-// }
-
-// //parse data
-// const parsedpatchData = cheerio.load(patchData);
 
