c47fbffdb2ee6d977c94ca5148dd2b16ae61bf34
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-05-07 17:59:48 -0400

    patch.gg

---
+++ Script/valorant.js
@@ -0,0 +1,26 @@
+//collapse
+document.addEventListener("DOMContentLoaded", function() {
+    var btn = document.getElementsByClassName("collapse");
+  
+    btn[0].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+
+  //fetch data from JSON
+  fetch("/JSON/valPatch.JSON")
+  .then(res => res.json())
+  .then(data => console.log(data))
+  console.log(data);
+
+  //populate page with containers based on JSON data
+
+//   function populatePatch(){
+
+//   }
