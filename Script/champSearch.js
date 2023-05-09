dff64e2b093b26f494e4a1a9abc5d40d90319b45
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-05-09 17:09:32 -0400

    JS

--- Script/champSearch.js
+++ Script/champSearch.js
@@ -12,14 +12,14 @@ function loadChamp(data){
     searchChamp = document.getElementById("block");
     for (i = 0; i < data.lolPatch.length; i++){
         var div = document.createElement("div");
-        div.style.width = 326 + "px";
+        div.style.width = 351 + "px";
         div.style.height = 105 + "px";
         div.style.backgroundColor = "#2C2C2C";
         div.style.borderRadius = 4 + "px";
         div.style.marginBottom = "10px";
         div.style.display = "flex";
         div.style.justifyContent = "center";
-        div.style.alignItems = "center";
+        div.style.alignItems = "left";
         div.style.textAlign = "left";
         div.innerHTML = 
         data.lolPatch[i].champName 
